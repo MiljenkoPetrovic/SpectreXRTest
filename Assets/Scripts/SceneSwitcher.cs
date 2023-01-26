eda3d7c03f190ece77e1505b18097f60ddc0596d
MiljenkoPetrovic <80465450+MiljenkoPetrovic@users.noreply.github.com>
2023-01-26 12:02:57 +0100

    Main menu scene select
    You can now play the game from the main menu.

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -5,5 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
